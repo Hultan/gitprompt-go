cc0e6b39f2f4d632364863e2cc5c96365014c13b
Per Hultqvist <per@softteam.se>
2020-07-09 09:15:00 +0200

    Fixed escape bug, and readme

--- README.md
+++ README.md
@@ -1,5 +1,76 @@
 # gitprompt-go
-A git prompt for Bash terminals.
 
-This started out as a copy of https://github.com/9ary/gitprompt-rs, but written in **go** instead of **rust**.
+##Usage
+This started out as a copy of gitprompt-rs (https://github.com/9ary/gitprompt-rs), but written in **go** instead of **rust**.
 
+By default it will produce a string like this (depending on the directory you are in) :
+
+    master↑0↓0|+0~3-0x0•0
+    
+ Explanation (copied from https://github.com/9ary/gitprompt-rs):
+ 
+- Branch info:
+  - `master`: name of the current branch, `:HEAD` in detached head mode
+  - `↑`: number of commits ahead of remote
+  - `↓`: number of commits behind remote
+- Work area:
+  - `+`: untracked (new) files
+  - `~`: modified files
+  - `-`: deleted files
+  - `x`: merge conflicts
+- `•`: staged changes
+
+What I have added to 9ary:s code is a json config file, where you can change the prompt to match your situation. For example you can change the symbols used, reorder the information displayed, and change colors etc. The config file should be placed in `~/.config/gitprompt-go/config.json`.
+
+A sample config file:
+ ```
+ {
+   "format": "$(BRANCH)$(AHEAD)$(BEHIND)$(SEPARATOR)$(UNTRACKED)$(MODIFIED)$(DELETED)$(UNMERGED)$(STAGED)",
+   "includeZeroValues" : false,
+   "separator": "|",
+   "promptPrefix": "(",
+   "promptSuffix": ")",
+   "branch" :  {
+     "prefix": "$(ESC)[34m",
+     "suffix": "$(ESC)[90m"
+   },
+   "ahead": {
+     "prefix": "↑",
+     "suffix": ""
+   },
+   "behind": {
+     "prefix": "↓",
+     "suffix": ""
+   },
+   "untracked": {
+     "prefix": "+",
+     "suffix": ""
+   },
+   "modified": {
+     "prefix": "~",
+     "suffix": ""
+   },
+   "deleted": {
+     "prefix": "-",
+     "suffix": ""
+   },
+   "unmerged": {
+     "prefix": "x",
+     "suffix": ""
+   },
+   "staged": {
+     "prefix": "•",
+     "suffix": ""
+   }
+ }
+```
+
+Use `$(ESC)` instead of `\x1b` or `\033`, it will be replaced by the code later.
+
+After following Cris Titus Tech:s video (https://www.youtube.com/watch?v=iaXQdyHRL8M) and adding `$(gitprompt-go)` to your shell prompt, and creating the config file `config.json` above in the `~/.config/gitprompt-go` folder ,your prompt should look something like this:
+
+![gitprompt-go](gitprompt.png)
+
+## Known issues
+
+* **gitprompt-rs** prompt work with ZSH, **gitprompt-go** probably doesn't. Since I don't use ZSH, I didn't bother to copy that code.
--- src/advancedprompt.go
+++ src/advancedprompt.go
@@ -1,5 +1,86 @@
 package main
 
+//# Reset
+//Color_Off='\033[0m'       # Text Reset
+//
+//# Regular Colors
+//Black='\033[0;30m'        # Black
+//Red='\033[0;31m'          # Red
+//Green='\033[0;32m'        # Green
+//Yellow='\033[0;33m'       # Yellow
+//Blue='\033[0;34m'         # Blue
+//Purple='\033[0;35m'       # Purple
+//Cyan='\033[0;36m'         # Cyan
+//White='\033[0;37m'        # White
+//
+//# Bold
+//BBlack='\033[1;30m'       # Black
+//BRed='\033[1;31m'         # Red
+//BGreen='\033[1;32m'       # Green
+//BYellow='\033[1;33m'      # Yellow
+//BBlue='\033[1;34m'        # Blue
+//BPurple='\033[1;35m'      # Purple
+//BCyan='\033[1;36m'        # Cyan
+//BWhite='\033[1;37m'       # White
+//
+//# Underline
+//UBlack='\033[4;30m'       # Black
+//URed='\033[4;31m'         # Red
+//UGreen='\033[4;32m'       # Green
+//UYellow='\033[4;33m'      # Yellow
+//UBlue='\033[4;34m'        # Blue
+//UPurple='\033[4;35m'      # Purple
+//UCyan='\033[4;36m'        # Cyan
+//UWhite='\033[4;37m'       # White
+//
+//# Background
+//On_Black='\033[40m'       # Black
+//On_Red='\033[41m'         # Red
+//On_Green='\033[42m'       # Green
+//On_Yellow='\033[43m'      # Yellow
+//On_Blue='\033[44m'        # Blue
+//On_Purple='\033[45m'      # Purple
+//On_Cyan='\033[46m'        # Cyan
+//On_White='\033[47m'       # White
+//
+//# High Intensity
+//IBlack='\033[0;90m'       # Black
+//IRed='\033[0;91m'         # Red
+//IGreen='\033[0;92m'       # Green
+//IYellow='\033[0;93m'      # Yellow
+//IBlue='\033[0;94m'        # Blue
+//IPurple='\033[0;95m'      # Purple
+//ICyan='\033[0;96m'        # Cyan
+//IWhite='\033[0;97m'       # White
+//
+//# Bold High Intensity
+//BIBlack='\033[1;90m'      # Black
+//BIRed='\033[1;91m'        # Red
+//BIGreen='\033[1;92m'      # Green
+//BIYellow='\033[1;93m'     # Yellow
+//BIBlue='\033[1;94m'       # Blue
+//BIPurple='\033[1;95m'     # Purple
+//BICyan='\033[1;96m'       # Cyan
+//BIWhite='\033[1;97m'      # White
+//
+//# High Intensity backgrounds
+//On_IBlack='\033[0;100m'   # Black
+//On_IRed='\033[0;101m'     # Red
+//On_IGreen='\033[0;102m'   # Green
+//On_IYellow='\033[0;103m'  # Yellow
+//On_IBlue='\033[0;104m'    # Blue
+//On_IPurple='\033[0;105m'  # Purple
+//On_ICyan='\033[0;106m'    # Cyan
+//On_IWhite='\033[0;107m'   # White
+//
+//And then use them like this in your script:
+//
+//#    .---------- constant part!
+//#    vvvv vvvv-- the code from above
+//RED='\033[0;31m'
+//NC='\033[0m' # No Color
+//printf "I ${RED}love${NC} Stack Overflow\n"
+
 import (
 	"strconv"
 	"strings"
@@ -18,13 +99,11 @@ func NewAdvancedPrompt(git *Git, config *Config) *AdvancedPrompt {
 }
 
 func (a *AdvancedPrompt) GetPrompt() string {
-	var result string
 	parts := strings.Split(a.Config.Format, "$(SEPARATOR)")
 
-	var part string
-
-	result = a.getPromptPart(parts[0])
+	result := a.getPromptPart(parts[0])
 	if len(parts)>1 {
+		var part string
 		for i := 1; i < len(parts); i++ {
 			part = a.getPromptPart(parts[i])
 			if len(result)>0 && len(part)>0 {
@@ -32,11 +111,16 @@ func (a *AdvancedPrompt) GetPrompt() string {
 			}
 		}
 	}
-	return a.Config.PromptPrefix + result + a.Config.PromptSuffix
+
+	return a.escape(a.Config.PromptPrefix + result + a.Config.PromptSuffix)
+}
+
+func (a *AdvancedPrompt) escape(text string) string {
+	return strings.ReplaceAll(text,"$(ESC)","\x1b")
 }
 
 func (a *AdvancedPrompt) getPromptPart(part string) string {
-	var result string = part
+	result := part
 
 	result = strings.ReplaceAll(result, "$(SEPARATOR)",a.Config.Separator)
 	branch := a.getBranch(a.Git, a.Config)
