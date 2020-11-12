f9c60f9077b43d48c7147b0b95a5a4b66b5bf44c
Per Hultqvist <per@softteam.se>
2020-11-12 18:30:15 +0100

    wtf

---
+++ cmd/gitprompt-go/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"gitprompt-go/internal"
+)
+
+func main() {
+	// Create the git prompt object
+	prompt:= internal.NewGitPrompt()
+	// Print the git prompt
+	fmt.Println(prompt.Prompt)
+}
